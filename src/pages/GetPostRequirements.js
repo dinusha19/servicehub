aa3a32cbdaf7ec6faabc5b53c171b552efca1d34
dinusha19 <sdinusha842@gmail.com>
2022-06-07 14:08:10 +0530

    added post details

--- src/pages/GetPostRequirements.js
+++ src/pages/GetPostRequirements.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-//import {Card} from 'react-bootstrap';  
-//import {requestForToken,onMessageListener} from './firebase'
+import {Card} from 'react-bootstrap';  
 
 
 const GetPostRequirements = () => {
@@ -13,7 +12,7 @@ const GetPostRequirements = () => {
             const response = await axios.get("http://localhost:9000/postrequirement/getPostRequirements");
             
             console.log(response.data);
-            //SetPostRequirement(response.data)
+            SetPostRequirement(response.data)
 
 
         } catch (error) {
@@ -27,7 +26,43 @@ const GetPostRequirements = () => {
     }, []);
 
     return(
-        <h1>Post requirenment details</h1>
+        <div>
+
+        <div >
+            {
+                PostRequirement.map((row) => {
+
+                    return (
+
+                        <Card style={{ width: '18rem', margin:'20px' }}>
+                        
+                            <Card.Body>
+                                <Card.Text>
+                                  <b>{row.category}</b> 
+                                </Card.Text>
+                                <Card.Text>
+                                  <b>{row.email}</b> 
+                                </Card.Text>
+                                <Card.Text>
+                                   {row.location}
+                                </Card.Text>
+                                <Card.Text>
+                                  <b>{row.phonenumber}</b> 
+                                </Card.Text>
+                                <Card.Text>
+                                   {row.requirementDetails}
+                                </Card.Text>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
+                            </Card.Body>
+                        </Card>
+
+
+                    )
+                })
+            }
+        </div>
+
+    </div>
     );
 
      
